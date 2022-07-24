10669b361daa578b62fe864c559a0f75a578f7e4
Mason Maziano <mason.k.maziano@gmail.com>
2022-07-24 11:22:27 +0100

    Initialized Guessing game

---
+++ while_loops.rb
@@ -0,0 +1,19 @@
+# Guessing Game using WHILE LOOPS
+friends = ["Dog", "Sergio", "Games", "Pizza", "Hammer"]
+
+friends.each_with_index do |friend, index|
+    puts "#{index} - #{friend}"
+end
+puts "Choose Wisely!"
+
+secret_word = friends.sample
+guess = ""
+
+while guess.upcase != secret_word.upcase
+    print "Enter your guess: "
+    guess = gets.chomp()
+end
+
+if guess = secret_word 
+    puts "You Won!"
+end 
