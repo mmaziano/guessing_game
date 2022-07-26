380a75e59114d6de8cc469e8635927463df2447b
Mason Maziano <mason.k.maziano@gmail.com>
2022-07-26 11:49:23 +0100

    Guessing game

--- while_loops.rb
+++ while_loops.rb
@@ -1,5 +1,5 @@
 # Guessing Game using WHILE LOOPS
-friends = ["Dog", "Sergio", "Games", "Pizza", "Hammer", "Wallet", "Flower"]
+friends = ["Dog", "Sergio", "Games", "Pizza", "Hammer", "Wallet", "Flower", "You", "Videos"]
 
 friends.each_with_index do |friend, index|
     puts "#{index} - #{friend}"
